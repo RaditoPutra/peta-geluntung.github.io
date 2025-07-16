1546f88c3de070c42151ff45e719ae093b64fd06
RaditoPutra <220711745@students.uajy.ac.id>
2025-07-16 12:23:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ const koordinatPolygonGeluntung = [
 ];
 
 L.polygon(koordinatPolygonGeluntung, {
-    color: 'green',
+    color: 'baby pink',
     fillColor: '#32a852',
     fillOpacity: 0.4
 }).addTo(mymap).bindPopup('Wilayah Geluntung');
